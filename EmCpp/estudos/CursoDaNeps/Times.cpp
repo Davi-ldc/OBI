420e321cd14513a9a9048d6778f22e8d41e2c620
Davi-ldc <96698943+Davi-ldc@users.noreply.github.com>
2025-04-27 11:00:57 -0300

    To ficando bom nisso

--- EmCpp/estudos/CursoDaNeps/Times.cpp
+++ EmCpp/estudos/CursoDaNeps/Times.cpp
@@ -12,33 +12,55 @@
 using namespace std;
 
 /*
-A logica aqui é sortear o vetor em Log n, ou seja cada indice vai sendo adicionado
-já de forma sorteada ai depois é so ir pegando do topo e adicionar aos times
-
+A logica aqui é sortear o vetor em N Log n e depois sortear por ordem alfabetica cada um dos times,
+nada de mais :)
 */
 
 int main(){
-    int N,T;//numero de pessoas e de times
+    int N,T;//numero de alunos e de times
     cin >> N >> T;
 
-    vector<string> nomes;
-    vector<int> habilidades;
+    vector<pair<string,int>> alunos;
     for (int i=0; i<N; i++){
         string nome;
         int habilidade;
         cin >> nome >> habilidade; 
 
-        habilidades.push_back(habilidade);
-        nomes.push_back(habilidade);
-
-        for (int i=0;i<habilidades.size();i++){
-            cout << nomes[i] << " " << habilidades[i] << endl;
-        }
-        
+        alunos.push_back({nome, habilidade});
     }
 
+    sort(alunos.begin(), alunos.end(), [](auto A, auto B){
+        // se A for maior que B então A vem primeiro
+        //Ou seja, os maiores valores vem primeiro e o vetor está ordenado de forma decrescente
+        return A.second > B.second;
+    });
 
-    vector<vector<string>> Times(3,vector<string>());
+    //debug
+    // for (auto p : alunos){
+    //     cout << p.first << " " << p.second << endl;
+    // }
+
+    //T vetores<string> vaizos
+    vector<vector<string>> times(T,vector<string>());
+    
+    for (int i=0; i<alunos.size();i++){//Vou botando o com maior habilidade no time 1, depois no 2, no 3 e repete
+        times[i%T].push_back(alunos[i].first);
+    }
+
+    //sorteia cada vetor de times em ordem alfabetica
+    for (int i =0;i<times.size();i++){
+        sort(times[i].begin(),times[i].end());
+    }
+
+    for (int i=0;i<times.size();i++){ //loop pra cada time
+        cout << "Time " << i+1 << endl;//i+1 por que não tem time 0
+        for (int j=0; j<times[i].size();j++){ //imprime cada string
+            cout << times[i][j] << endl;
+            if(j==times[i].size()-1){
+                cout << endl;//se for a ultima da um \n a mais
+            }
+        }
+    }
 
     return 0;
 }
