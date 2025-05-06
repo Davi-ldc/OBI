a6f3fb5d3792df2470df8a5e6d2690a114fafabb
Davi-ldc <96698943+Davi-ldc@users.noreply.github.com>
2025-05-05 21:08:28 -0300

    .

--- EmCpp/estudos/CursoDaNeps/estrategiasDeBusca/Sudoku.cpp
+++ EmCpp/estudos/CursoDaNeps/estrategiasDeBusca/Sudoku.cpp
@@ -8,7 +8,29 @@ using namespace std;
 // Cada subgrade 3x3 deve conter todos os números de 1 a 9, sem repetição.
 
 /*
-Vamosss lá, a ideia aqui é 
+Vamosss lá, Vamos pensar do jeito burro primeiro. Imagina que agnt tem 17 numeros já prenchidos
+e quer achar o proximo, como humano mesmo, poderiamos só ir testando e ver se o numero é valido
+
+Mas pera, quando que ele é valido? Dado o indice i,j um número é valido se na linha não tiver nenhum numero
+igual a ele ou seja pra cada elemento em M[i] nenhum pode ser igual ao numero desejado. Da até pra cmc com o set
+{1,2,3,4,5,6,7,8,9} e ir iterando sobre M[i][X] tirando os valores k do set (o (log N)) E o mesmo para M[X][j]
+
+Ta, mais ainda falta chechar o quadrante. Pensa neles como uma matriz:
+M -> 
+{
+    {1, 2, 3},
+    {4, 5, 6},
+    {7, 8, 9}
+}
+repara que é so pegar a posição i/3 e j/3 nessa matrix pra achar o quadrante
+
+tipo: (é o mesmo na vertical)
+0 0 0 1 1 1 2 2 2 index
+0 1 2 3 4 5 6 7 8 posição; Ta já sabemos checar se um numero é valido.
+
+Pra cada iteração eu poderia pegar meu set com os numeros possiveis, e ver ir deletando os que aparecem
+na linha; na coluna; no quadrante. e se não tiver nenhum faz o backtrack 
+
 
 */
 
