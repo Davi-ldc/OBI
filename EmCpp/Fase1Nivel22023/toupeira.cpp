f29826bc36b1f2e08efde6a129ba0a4c4f1b4d2c
Davi-ldc <96698943+Davi-ldc@users.noreply.github.com>
2025-05-12 21:17:42 -0300

    Voltando a ativa

---
+++ EmCpp/Fase1Nivel22023/toupeira.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    int S, T;//número de salões, número de tuneis
+    cin >> S >> T;
+
+    return 0;
+}
--- EmCpp/estudos/CursoDaNeps/Backtracking/NRainhas.cpp
+++ EmCpp/estudos/CursoDaNeps/Backtracking/NRainhas.cpp
@@ -13,7 +13,60 @@ passa pra proxima, se não retorna
 Mas nesse caso, ele quer o numero de soluções. 
 */
 
-bool isVal(vector<vector<int>> M, int i, int j){
+//testa se é valido posicionar uma rainha em i, j
+bool isVal(vector<vector<int>>& M, int i, int j){
+    
+    //linha
+    for(int k = 0; k < M.size(); k++){
+        if(M[i][k] == 1) return false;
+    }
+
+    //coluna
+    for(int k = 0; k < M.size(); k++){
+        if(M[k][j] == 1) return false;
+    }
+
+    //diagonal
+    for(int k = 0; k < M.size(); k++){
+        //cima
+        if(i+k < M.size() && j+k < M.size()){
+            if(M[i+k][j+k] == 1) return false;
+        }
+        //baixo
+        if(i-k >= 0 && j-k >= 0){
+            if(M[i-k][j-k] == 1) return false;
+        }
+        //cima esquerda
+        if(i+k < M.size() && j-k >= 0){
+            if(M[i+k][j-k] == 1) return false;
+        }
+        //cima direita
+        if(i-k >= 0 && j+k < M.size()){
+            if(M[i-k][j+k] == 1) return false;
+        }
+    }
+
+    return true;
+}
+
+int Nsolucoes(vector<vector<int>>& M, int i, int j){
+
+    //se chegou na ultima linha
+    if(i == M.size()){
+        return 1;
+    }
+
+    int resposta = 0;
+
+    for(int k = 0; k < M.size(); k++){
+        if(isVal(M, i, k)){
+            M[i][k] = 1;
+            resposta += Nsolucoes(M, i+1, k);
+            M[i][k] = 0;
+        }
+    }
+
+    return resposta;
 
 }
 
@@ -23,15 +76,16 @@ int main(){
     cin >> N;
 
 
-    vector<vector<int>> M(N, vector<int>(N)); 
-    for (int i =0;i<N;i++){
-        for(int j=0;j<N;j++){
-            cin >> M[i][j];
-        }
+    //tabuleiro
+    vector<int> sizes(N);
+    for (int i = 0; i < N; i++){
+        cin >> sizes[i];
     }
 
-
-
+    for (int i =0; i<N; i++){
+        vector<vector<int>> M(N, vector<int>(sizes[i],0));
+        cout << Nsolucoes(M, 0, 0) << endl;
+    }
 
     return 0;
 }
