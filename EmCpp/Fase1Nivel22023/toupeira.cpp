31a022b36f8deaa9775f1c28464fca5b0cc3856e
Davi-ldc <96698943+Davi-ldc@users.noreply.github.com>
2025-05-21 20:48:49 -0300

    Aprendendo Grafos

--- EmCpp/Fase1Nivel22023/toupeira.cpp
+++ EmCpp/Fase1Nivel22023/toupeira.cpp
@@ -8,7 +8,43 @@ int main(){
     int S, T;//número de salões, número de tuneis
     cin >> S >> T;
 
-    
+    vector<vector<int>> M(S, vector<int>(S,0));
+
+    for (int i =0; i<T; i++){
+        int a, b;
+        cin >> a >> b;
+        M[b-1][a-1] = 1;
+    }
+
+    //debug
+    // for(int i = 0; i<S; i++){
+    //     for (int j=0;j<S;j++){
+    //         cout << M[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    int P; cin >> P;
+
+    int resposta =0;
+    for (int i =0; i<P;i++){
+        int size; cin >> size;
+        vector<int> passeio(size,0);
+
+        //preenche o vetor
+        for(int j=0;j<size;j++){cin>>passeio[j];}
+
+        bool valido = true;
+        for (int k=0; k<size-1; k ++){
+            if(M[passeio[k]-1][passeio[k+1]-1] != 1 && M[passeio[k+1]-1][passeio[k]-1] != 1 ){
+                valido = false;
+                break;
+            }
+        }
+        if(valido){resposta++;}
+    }
+
+    cout << resposta;
 
     return 0;
 }
