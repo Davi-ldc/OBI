35041c301f3e2c1e6bb377da639575aff30c4ac3
Davi-ldc <96698943+Davi-ldc@users.noreply.github.com>
2025-06-01 17:09:30 -0300

    .

--- EmCpp/Fase1Nivel22022/chuva.cpp
+++ EmCpp/Fase1Nivel22022/chuva.cpp
@@ -3,6 +3,17 @@
 
 using namespace std;
 
+/*
+O problema é dado uma lista N, quantas sublistas R tem soma = Sd
+se todos os numeros fossem positivos dava pra resolver com sliding window, 2 ponteiros parados no começo
+J sempre avança e soma += [j] ai enquanto currentSum for > Sd e i<=j i++ soma -= V[i].
+acho que da pra adaptar o algoritimo pra receber 0, ou usar prefix sum mas nesse caso
+a implementaçaõ em O(N²/2) passou por causa do check:
+if(soma > Sd){
+        break;
+} 
+*/
+
 int main(){
 
     int N, Sd; cin >> N >> Sd;
@@ -14,16 +25,38 @@ int main(){
 
     int resposta = 0;
 
-    vector<int> prefixSum(N);
-    int soma = 0;
-    for(int i =0; i < N; i++){
-        soma += V[i]
-        prefixSum[i] = soma; 
+    for(int i =0; i<N; i++){
+        int soma = 0;
+        for(int j=i; j<N; j++){
+            soma += V[j];
+            if (soma == Sd){
+                resposta++;
+            }
+            if(soma > Sd){
+                break;
+            }
+        }
     }
 
-    //repara que a soma de a até b é prefix[b] - prefix[a]
+    // int soma = 0;
+    // int i = 0;
+    // int j = 0;
+    // int resposta = 0;
 
-    //ai ele quer saber de quantas formas podemos somar R
+    // //Se J chegou ao final, não  tem mais nenhuma soma que some
+    // while (j<V.size()){
+        
+    //     soma += V[j];
+
+    //     if(soma == Sd){cout << "i: " << i << " j: " << j << " Soma: " << soma << endl; resposta++;}
+    //     while ( soma > Sd && i <= j){
+    //         soma -= V[i];
+    //         i++;
+    //         if(soma == Sd){cout << "i: " << i << " j: " << j << " Soma: " << soma << endl; resposta++;}
+    //     }
+
+    //     j++;
+    // }
 
     cout << resposta;
     return 0;
